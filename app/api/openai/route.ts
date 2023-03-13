f1d60b904c7dabd7335d3ad614db4cad7763b718
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-03-13 23:45:06 +0800

    Update route for getting chat

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,12 +1,14 @@
-import { Configuration, OpenAIApi } from 'openai';
-import { NextResponse } from 'next/server';
+import { Configuration, CreateChatCompletionRequest, OpenAIApi } from 'openai';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
     return new Response('Hello, Next.js GET!');
 }
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
     try {
+        const json: CreateChatCompletionRequest = await req.json();
+
         const openai = new OpenAIApi(
             new Configuration({
                 apiKey: process.env.OPENAI_API_KEY,
@@ -15,17 +17,14 @@ export async function POST(req: Request) {
 
         const completion = await openai.createChatCompletion({
             model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    content: 'Hello',
-                    role: 'user',
-                },
-            ],
+            messages: json.messages,
         });
 
-        const { data } = completion;
+        const {
+            data: { choices, id },
+        } = completion;
 
-        return new NextResponse(JSON.stringify(data), {
+        return new NextResponse(JSON.stringify({ choices, id }), {
             status: 200,
         });
     } catch (error) {
