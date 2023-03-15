fae5026d26ad795a8b83afdf000d8e4ca33e4ed8
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-03-15 23:09:00 +0800

    Add loading in send button

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -13,13 +13,20 @@ const Button = ({
     className,
     onClick,
     children,
+    disabled,
 }: {
     onClick: () => void;
     className: string;
     children: React.ReactNode;
+    disabled?: boolean;
 }) => {
     return (
-        <button type="button" className={className} onClick={onClick}>
+        <button
+            type="button"
+            className={className}
+            onClick={onClick}
+            disabled={disabled}
+        >
             {children}
         </button>
     );
@@ -107,15 +114,37 @@ export default function Page({}: Props) {
                     style={{ resize: 'none' }}
                     value={text}
                 />
-                {!submitting && (
-                    <Button
-                        onClick={handleClick}
-                        className=" absolute right-6 top-1/2 -translate-y-1/2 rounded bg-blue-600
-                    px-3 py-1"
-                    >
-                        Send
-                    </Button>
-                )}
+
+                <Button
+                    onClick={handleClick}
+                    className=" absolute right-6 top-1/2 inline-flex -translate-y-1/2 items-center gap-3 rounded bg-blue-600
+                    px-5 py-2"
+                    disabled={submitting}
+                >
+                    {submitting ? 'Processing' : 'Send'}
+                    {submitting && (
+                        <svg
+                            className="ml-3 h-5 w-5 animate-spin text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                            ></circle>
+                            <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                        </svg>
+                    )}
+                </Button>
             </div>
         </section>
     );
