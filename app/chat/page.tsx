558c457012a255c58c59a8bb2e0facdf5aebecc7
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-03-21 23:55:26 +0800

    Update chat window

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 type Props = {};
 
@@ -13,13 +14,20 @@ const Button = ({
     className,
     onClick,
     children,
+    disabled,
 }: {
     onClick: () => void;
     className: string;
     children: React.ReactNode;
+    disabled?: boolean;
 }) => {
     return (
-        <button type="button" className={className} onClick={onClick}>
+        <button
+            type="button"
+            className={className}
+            onClick={onClick}
+            disabled={disabled}
+        >
             {children}
         </button>
     );
@@ -27,7 +35,15 @@ const Button = ({
 
 export default function Page({}: Props) {
     const [text, setText] = useState<string>('');
-    const [chat, setChat] = useState<any[]>([]);
+    const [chat, setChat] = useState<any[]>([
+        // { role: 'user', content: 'Type a long lorem ipsum' },
+        // {
+        //     role: 'assistant',
+        //     content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        //     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        //     `,
+        // },
+    ]);
     const [submitting, setSubmitting] = useState<boolean>(false);
 
     const handleClick = async () => {
@@ -56,7 +72,17 @@ export default function Page({}: Props) {
                     },
                 ];
 
+                const conversationObj = {
+                    id: res.id,
+                    conversation: newConversation,
+                    title: res.id,
+                };
+
                 setChat(newConversation);
+                localStorage.setItem(
+                    `chat-${res.id}`,
+                    JSON.stringify(conversationObj)
+                );
             }
         }
         setSubmitting(false);
@@ -64,35 +90,62 @@ export default function Page({}: Props) {
 
     const textAreaOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const { value } = e.target;
-        if (value.length > 0) {
-            setText(value);
-        }
+        setText(value);
     };
 
     return (
-        <section className=" mx-auto h-screen max-w-[80%] px-12">
-            <div className="mb-10 h-[80vh] w-full overflow-y-auto">
+        <section className="mx-auto h-screen max-h-screen max-w-[80%] p-12 ">
+            <div className="mb-10 h-[75vh] w-full overflow-y-auto">
                 <div className="flex flex-col gap-5">
                     {chat &&
                         chat.map((message, index) => (
                             <div
                                 key={index}
-                                className={`flex flex-row gap-2 whitespace-pre-wrap ${
+                                className={`relative flex flex-row items-center gap-2 whitespace-pre-wrap ${
                                     message?.role === 'user'
                                         ? 'justify-end'
                                         : 'justify-start'
                                 }`}
                             >
                                 {message?.role && (
-                                    <p className="font-semibold capitalize">
-                                        {message?.role}:
-                                    </p>
+                                    <>
+                                        {message?.role !== 'user' && (
+                                            <div className="absolute left-0 top-0 z-10 h-8 w-8 rounded-full bg-gradient-to-r from-red-400 to-blue-500"></div>
+                                        )}
+                                    </>
                                 )}
 
                                 {message?.content && (
-                                    <p className="text-gray-300">
-                                        {message.content}
-                                    </p>
+                                    <div
+                                        className={`flex flex-col
+                                    ${
+                                        message?.role === 'user'
+                                            ? 'items-end'
+                                            : 'items-start'
+                                    }
+                                    `}
+                                    >
+                                        <div
+                                            className={`${
+                                                message?.role === 'user'
+                                                    ? ' mr-2 rounded-full rounded-br-none bg-blue-600'
+                                                    : ' rounded-4xl ml-10 mr-2 rounded-3xl rounded-tl-none bg-gray-600 '
+                                            } max-w-[85%] px-5 py-2`}
+                                        >
+                                            {message?.content}
+                                        </div>
+                                    </div>
+                                )}
+
+                                {message?.role === 'user' && (
+                                    <div className="relative h-8 w-8">
+                                        <Image
+                                            src="/images/user-image.png"
+                                            fill
+                                            alt="User"
+                                            className="absolute left-0 top-0 z-10 h-8 w-8 rounded-full"
+                                        />
+                                    </div>
                                 )}
                             </div>
                         ))}
@@ -106,16 +159,44 @@ export default function Page({}: Props) {
                     onChange={textAreaOnChange}
                     style={{ resize: 'none' }}
                     value={text}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter' && !e.shiftKey) {
+                            e.preventDefault();
+                            handleClick();
+                        }
+                    }}
                 />
-                {!submitting && (
-                    <Button
-                        onClick={handleClick}
-                        className=" absolute right-6 top-1/2 -translate-y-1/2 rounded bg-blue-600
-                    px-3 py-1"
-                    >
-                        Send
-                    </Button>
-                )}
+
+                <Button
+                    onClick={handleClick}
+                    className=" absolute right-6 top-1/2 inline-flex -translate-y-1/2 items-center gap-3 rounded bg-blue-600
+                    px-5 py-2"
+                    disabled={submitting}
+                >
+                    {submitting ? 'Processing' : 'Send'}
+                    {submitting && (
+                        <svg
+                            className="ml-3 h-5 w-5 animate-spin text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                            ></circle>
+                            <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                        </svg>
+                    )}
+                </Button>
             </div>
         </section>
     );
