55d603166dd7756dd5bb046a455d29d3a8978509
John Pevien Laguindam <johnpevienwork@gmail.com>
2023-04-01 01:45:24 +0800

    Update styles for home and settings link

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,4 +1,5 @@
 import Conversations from '../../components/Conversations';
+import Link from 'next/link';
 
 import '../globals.css';
 
@@ -31,30 +32,20 @@ export default function RootLayout({
                         <nav>
                             <ul className="space-y-2">
                                 <li>
-                                    <a
-                                        href="#"
+                                    <Link
+                                        href="/"
                                         className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                                     >
-                                        <span className="ml-3 flex-1 whitespace-nowrap">
-                                            Home
-                                        </span>
-                                        {/* <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                                        Home
+                                        {/* <span className="ml-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300">
                                             Pro
                                         </span> */}
-                                    </a>
+                                    </Link>
                                 </li>
                                 <li>
-                                    <a
-                                        href="#"
-                                        className="flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                                    >
-                                        <span className="ml-3 flex-1 whitespace-nowrap">
-                                            Settings
-                                        </span>
-                                        {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                                            3
-                                        </span> */}
-                                    </a>
+                                    <button className="flex w-full items-center rounded-lg p-2 text-left text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                        Settings
+                                    </button>
                                 </li>
                             </ul>
                         </nav>
